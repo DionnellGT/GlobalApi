7382bd19a057a6dfd3326925571eed89f1138c8e
Dionnell Riquelme <driquelme@globalterrenos.cl>
2026-07-03 15:36:34 -0400

    feat(campaigns): integrate Template entity into CampaignsModule and CampaignsService for enhanced campaign management

--- src/campaigns/campaigns.module.ts
+++ src/campaigns/campaigns.module.ts
@@ -4,11 +4,12 @@ import { MailModule } from '../mail/mail.module';
 import { RecipientsModule } from '../recipients/recipients.module';
 import { Campaign } from './entities/campaign.entity';
 import { SendLog } from '../recipients/entities/send-log.entity';
+import { Template } from '../templates/entities/template.entity';
 import { CampaignsService } from './campaigns.service';
 import { CampaignsController } from './campaigns.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Campaign, SendLog]), MailModule, RecipientsModule],
+  imports: [TypeOrmModule.forFeature([Campaign, SendLog, Template]), MailModule, RecipientsModule],
   controllers: [CampaignsController],
   providers: [CampaignsService],
   exports: [CampaignsService],
