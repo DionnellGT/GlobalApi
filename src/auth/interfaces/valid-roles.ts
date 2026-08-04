de1ce65df0af30ea44e8fca58bb94b7abb4a060d
Dionnell Riquelme <driquelme@globalterrenos.cl>
2026-08-04 15:08:37 -0400

    feat(validRoles): add additional roles for emailMasivo, leads, and listaPrecios

--- src/auth/interfaces/valid-roles.ts
+++ src/auth/interfaces/valid-roles.ts
@@ -5,4 +5,7 @@ export enum ValidRoles {
     admin = 'admin',
     superUser = 'super-user',
     user = 'user',
+    emailMasivo = 'emailMasivo',
+    leads = 'leads',
+    listaPrecios = 'listaPrecios',
 }
